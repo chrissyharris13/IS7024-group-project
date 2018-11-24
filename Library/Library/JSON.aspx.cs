79e1b60d18917fab0624063b1552fa086559bb35
Constance Landrum <landruce@ucmail.uc.edu>
2018-11-24 18:10:39 -0500

    added library data

---
+++ Library/Library/JSON.aspx.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Library
+{
+    public partial class Json_Library : System.Web.UI.Page
+    {
+       protected void Page_Load(object sender, EventArgs e)
+        {
+
+         using (var webClient = new WebClient())
+            {
+                string rawData =
+                    webClient.DownloadString("https://data.cityofchicago.org/api/views/izv6-vdkm/rows.json?accessType=DOWNLOAD");
+
+            }
+
+         
+              
+            
+        }
+    }
+}
---
+++ Library/Library/Library.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Library
+{
+    public class Library
+    {
+        private int id;
+        string Rank;
+        string Title;
+        string Author;
+        string PubYear;
+        string catalogrecords;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Rank1
+        {
+            get
+            {
+                return Rank;
+            }
+
+            set
+            {
+                Rank = value;
+            }
+        }
+
+        public string Title1
+        {
+            get
+            {
+                return Title;
+            }
+
+            set
+            {
+                Title = value;
+            }
+        }
+
+        public string Author1
+        {
+            get
+            {
+                return Author;
+            }
+
+            set
+            {
+                Author = value;
+            }
+        }
+
+        public string PubYear1
+        {
+            get
+            {
+                return PubYear;
+            }
+
+            set
+            {
+                PubYear = value;
+            }
+        }
+
+        public string Catalogrecords
+        {
+            get
+            {
+                return catalogrecords;
+            }
+
+            set
+            {
+                catalogrecords = value;
+            }
+        }
+    }
+}
+    
