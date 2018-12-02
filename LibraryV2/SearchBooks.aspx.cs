0449a598535c2b70292bd6cde2b2d04f72934768
Molly Zweig <molly.zweig@uc.edu>
2018-12-02 18:04:50 -0500

    Updating index page, removed old web service link

--- LibraryV2/SearchBooks.aspx.cs
+++ LibraryV2/SearchBooks.aspx.cs
@@ -21,12 +21,6 @@ namespace LibraryV2
                 List<LibraryRanking> libraryList = JsonConvert.DeserializeObject<List<LibraryRanking>>(rawJSON);
                 Console.WriteLine(libraryList.Count);
             }
-            //using (var webClient = new WebClient())
-            //{
-                //This is where our second data source will go.  We intend to use https://openlibrary.org/dev/docs/api/covers to show book covers when a title is put into the web form.   
-                //String rawData =
-                //webClient.DownloadString("Link to web service will go here");
-            //}
         }
 
         protected void BltdListDescr_Click(object sender, BulletedListEventArgs e)
