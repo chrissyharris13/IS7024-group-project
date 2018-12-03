ad41e7083f9dc730eb300f8513cee1df77b207ed
Christina Harris <harri2c4@mail.uc.edu>
2018-12-02 20:08:39 -0500

    Attempted a web service

---
+++ LibraryV2/MoviesWebService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="MoviesWebService.asmx.cs" Class="LibraryV2.MoviesWebService" %>
---
+++ LibraryV2/MoviesWebService.asmx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Services;
+using Newtonsoft.Json;
+
+namespace LibraryV2
+{
+    /// <summary>
+    /// Summary description for MoviesWebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class MoviesWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string LookUpMovie(String title)
+        {
+            string returnValue = "No title found";
+
+            MovieCollection movieCollection;
+
+            IDictionary<String, Int32> titleCount = new Dictionary<String, Int32>();
+
+
+            using (var webClient = new WebClient())
+            {
+                string rawData =
+                    webClient.DownloadString("https://data.cityofchicago.org/resource/6gjb-wqjd.json");
+
+                List<Movie> moviesList = JsonConvert.DeserializeObject<List<Movie>>(rawData);
+            }
+
+            foreach (Movie movie in movieCollection.Movies1)
+            {
+                if (titleCount.ContainsKey(movie.Title))
+                {
+                    int currentCount = titleCount[movie.Title];
+                    currentCount++;
+                    titleCount[movie.Title] = currentCount;
+                }
+                else
+                {
+                    //title does not exist; add it with a default count of "1"
+                    titleCount.Add(movie.Title, 1);
+                }
+            }
+
+
+
+            return returnValue;
+        }
+    }
+}
